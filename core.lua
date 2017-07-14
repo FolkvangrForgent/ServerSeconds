fb713a9d97b29646edbbf4d3c5e7ad6c988dfb4e
Zore12345 <makerbot11@gmail.com>
2017-07-14 11:04:51 -0500

    Update core.lua
    added comments
    added basic header
    fixed error with code that on low fps machines could have lead to time being off.

--- core.lua
+++ core.lua
@@ -1,9 +1,13 @@
-SLASH_SERVERTIME1, SLASH_SERVERTIME2 = '/showservertime','/stime'
+--Writen by Forgent/Zore12345
+--TD: write an algorithm to decide when to reset severseconds to 0. Should be based on seconds since last updated and elapsed time (or fps).
+
+
+SLASH_EXPANDEDSERVERTIME1, SLASH_EXPANDEDSERVERTIME2 = '/showservertime','/stime'
 local function slashServerTimeStuff(msg, editbox)
 	local hour,minute = GetGameTime();
 	print("Current Server Time is "..string.format("%02i:%02i:%02i",hour,minute, math.floor(serverseconds + .5)));
 end
-SlashCmdList["SERVERTIME"] = slashServerTimeStuff;
+SlashCmdList["EXPANDEDSERVERTIME"] = slashServerTimeStuff;
 
 local serverseconds = 0;
 local previousminute = 0;
@@ -11,13 +15,16 @@ local function onUpdate(self,elapsed)
 	serverseconds = serverseconds + elapsed;
 	local hour,minute = GetGameTime();
 	if serverseconds >= 60 then
-		serverseconds = serverseconds%60;
+		serverseconds = serverseconds - 60;
 	end
-	if minute > previousminute and elapsed < 1 then
+	--below is a naieve method used to decided if the seconds should be reset to 0.
+	if minute > previousminute then
 		previousminute = minute;
-		serverseconds=0;
+		if elapsed < 1 then
+			serverseconds=0;
+		end
 	end
 end
- 
-local f = CreateFrame("Frame")
-f:SetScript("OnUpdate", onUpdate)
+
+local onUpdateFrame = CreateFrame("Frame");
+onUpdateFrame:SetScript("OnUpdate", onUpdate);
